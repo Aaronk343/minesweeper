b8eb1ef1764dd4c5c0025acab8c4f89dbb35d452
Addalene80 <addalene80@gmail.com>
2026-04-25 10:26:01 -0400

    finish code

--- main (1).py
+++ main (1).py
@@ -62,28 +62,28 @@ class board_class():
         return returnstr
 
     def reveal_tiles(self, x, y):
-        if self.player_board[y][x]==1:
+        if self.player_board[y][x] == 1:
             return True
-        
-        
-        if self.board[y][x]=="M":
+
+        if self.board[y][x] == "M":
             return False
-        self.player_board[y][x]=1
-        self.selectAbleSpots-=1
-        if self.board[y][x]==0:
-            for i in range(x-1,x+2):
-                if i>=0 and i<self.board_size:
-                    if y-1>=0:
-                        self.reveal_tiles(i, y-1)
-                    if y+1<self.board_size:
-                        self.reveal_tiles(i, y+1)
-                if i-1>=0:
-                    self.reveal_tiles(i-1, y)
-                if i+1<self.board_size:
-                    self.reveal_tiles(i+1, y)
-            return True 
-        else:
-            return True   
+
+        self.player_board[y][x] = 1
+        self.selectAbleSpots -= 1
+
+        if self.board[y][x] == 0:
+            for dx in range(-1, 2):
+                for dy in range(-1, 2):
+                    nx = x + dx
+                    ny = y + dy
+
+                    if dx == 0 and dy == 0:
+                        continue
+
+                    if 0 <= nx < self.board_size and 0 <= ny < self.board_size:
+                        self.reveal_tiles(nx, ny)
+
+        return True
 
     def add_mines(self, x, y):
         count=0
@@ -146,7 +146,11 @@ class board_class():
         self.player_board[box_y][box_x]=1
         if self.board[box_y][box_x]=="M":
             self.handle_game_over()
-        #handle win
+            return
+        elif self.selectAbleSpots==0:
+            self.win=True
+            self.handle_game_over()
+            return
         self.draw_board()
         
     def handle_click_flag(self, x, y):
@@ -176,104 +180,6 @@ class board_class():
             self.arrow.write("You Win")
         else:
             self.arrow.write("You Lose")
-# def mine_sweeper(mine_count, board_size, starting_x, starting_y):
-#     board=[[0 for i in range(board_size)] for j in range(board_size)]
-    
-#     while mine_count!=0:
-#         x=r.randint(0,board_size-1)
-#         y=r.randint(0,board_size-1)
-#         if board[y][x]==0:
-#             if y==starting_y and x==starting_x:
-#                 continue
-#             mine_count-=1
-#             board[y][x]="M"
-#     for i in range(x-1,x+2):
-
-#         count=0
-#         if y>=0 and y<board_size:
-#             if i-1>=0 and board[y][i-1]=="M":
-#                 count+=1
-#             if i+1<board_size and board[y][i+1]=="M":
-#                 count+=1
-#         if y-1>=0 and board[y-1][i]=="M":
-#             count+=1
-#         if y+1<board_size and board[y+1][i]=="M":
-#             count+=1
-#         board[y][i]=count
-    
-#     for j in range(board_size):
-#         for k in range(board_size):
-
-#     return board        
-# """            
-        # if i-1>=0 and j-1>=0 and board[i-1][j-1]=="M":
-        #     count+=1
-        # if  i-1>=0 and board[i-1][j]=="M":
-        #     count+=1
-        # if i-1>=0 and j+1<board_size and board[i-1][j+1]=="M":
-        #     count+=1
-        # if j-1>=0 and board[i][j-1]=="M":
-        #     count+=1
-        # if j+1<board_size and board[i][j+1]=="M":
-        #     count+=1        
-        # if i+1<board_size and j-1>=0 and board[i+1][j-1]=="M":
-        #     count+=1
-        # if i+1<board_size and board[i+1][j]=="M":
-        #     count+=1    
-        # if i+1<board_size and j+1<board_size and board[i+1][j+1]=="M":
-        #     count+=1
-"""        
-        
-
-def reveal_tiles(x, y, player_board, board, board_size):
-    player_board[y][x]=1
-    if board[y][x]=="M":
-        return False
-    if board[y][x]==0:
-        for i in range(x-1,x+2):
-            if i>=0 and i<board_size:
-                if y-1>=0:
-                    reveal_tiles(i, y-1, player_board, board, board_size)
-                if y+1<board_size:
-                    reveal_tiles(i, y+1, player_board, board, board_size)
-            if i-1>=0:
-                reveal_tiles(i-1, y, player_board, board, board_size)
-            if i+1<board_size:
-                reveal_tiles(i+1, y, player_board, board, board_size)
-        return True 
-    else:
-        return True
-"""        
-
-
-
-# def main():
-#     game_over=False
-#     game_win=False
-#     board_size=int(input("What board size do you want: "))
-#     bombs=int((board_size**2)*0.16)
-#     starting_x=int(input("What do you want your starting x-cordinate to be: "))
-#     starting_y=int(input("What do you want your starting y-cordinate to be: "))
-#     player_board=[[0 for i in range(board_size)] for j in range(board_size)]
-    
-#     board=mine_sweeper(bombs, board_size, starting_x, starting_y)
-#     reveal_tiles(starting_x, starting_y, player_board, board, board_size)
-#     print_grid(board, player_board)
-#     while not game_over:
-#         x=int(input("What x-coordinate to do want to reveal: "))
-#         y=int(input("What y-coordinate to do want to reveal: "))
-#         reveal_tiles(x,y, player_board, board, board_size)
-#         print_grid(board, player_board)
-#         print_grid(board, board)
-#         if player_board[y][x]=="M":
-#             game_state=="Game Over"
-    
-#     if game_win:
-#         print("You win")
-#     else:
-#         print("You lose")
-    
-# main()
 
 def main():
     
